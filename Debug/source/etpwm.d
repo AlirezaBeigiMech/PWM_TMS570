a889ea4a0e53c1f41b66543e7beda7dd2059f03e
alirezabeigi <alireza_beigi@sfu.ca>
2023-01-13 17:32:52 -0800

    Initial commit

---
+++ Debug/source/etpwm.d
@@ -0,0 +1,63 @@
+# FIXED
+
+source/etpwm.obj: ../source/etpwm.c
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/etpwm.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_etpwm.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/sys_common.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/hal_stdtypes.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+source/etpwm.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/pinmux.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_pinmux.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/sys_vim.h
+source/etpwm.obj: C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_vim.h
+
+../source/etpwm.c:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/etpwm.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_etpwm.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/sys_common.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/hal_stdtypes.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/pinmux.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_pinmux.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/sys_vim.h:
+
+C:/Users/AlirezaBeigi/workspace_v13/PWM1/include/reg_vim.h:
+
